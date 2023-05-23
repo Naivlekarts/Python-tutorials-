1881cd2ed5a1653f6324e663f933ee3b7e54a34c
Naivlekarts <134359691+Naivlekarts@users.noreply.github.com>
2023-05-23 08:25:42 +0300

    Add files via upload
    This is my first ever commit message

---
+++ Untitled-1.py
@@ -0,0 +1,133 @@
+print("Hello World!")
+if 5>2:
+    print("Five is greater than Two")
+    x=3
+    y="welcome to internet of things"
+    print(x)
+    print(y)
+    #This is a comment
+    print("I love IOT")
+    """
+    this is a comment
+    written in 
+    more than a single line
+    """
+    print("My python journey begins here")
+    x=6
+    y="Sally"
+    print(x)
+    print(y)
+    x=str(5)
+    y=int(5)
+    z=float(5)
+    print(x)
+    print(y)
+    print(z)
+    x=4
+    y="naivlek"
+    print(type(x))
+    print(type(y))
+    x='John'
+    print(x)
+    my_variable_name="naivlek"
+    print(my_variable_name)
+    fruits=["apple","orange","banana"]
+    x,y,z=fruits
+    print(x)
+    print(y)
+    print(z)
+    x="python"
+    y="is"
+    z="awesome"
+    print(x+y+z)
+    x=5
+    y=10
+    print(x+y)
+    x=5
+    y="Kelvian"
+    print(x,y)
+    x="  awesome"
+    def myfunc():
+        print("Python is "+x)
+    myfunc()
+    def myfunc():
+        global x
+        x="fantastic"
+    myfunc()
+    print("Pyrthon is "+ x)    
+    x="Welcome to Python"
+    #Display x:
+    print(x)
+    #Display the data type of x:
+    print(type(x))
+    x=20.678
+    #Dispaly x:
+    print (x)
+    #Dispaly the data type of x:
+    print(type(x))
+    y=2j
+    print(y)
+    print(type(y))
+    x=range(6)
+    print(x)
+    print(type(x))
+    x={'Name':'John','Age':36}
+    print(x)
+    print(type(x))
+    x=False
+    print(x)
+    print(type(x))
+    x=memoryview(bytes(7))
+    print(x)
+    print(type(x))
+    x=list(('orange','apple','cherry'))
+    print(x)
+    print(type(x))
+    x=bool(6)
+    print(x)
+    print(type(x))
+    y=3e4
+    print(type(y))
+    x=2
+    y=1.6
+    z=4j
+    #Convert int to float
+    a=float(x)
+    #convert float to complex
+    b=complex(y)
+    #convert float to int
+    c=int(y)
+    print(a)
+    print(b)
+    print(c)
+    #Display their data types
+    print(type(a))
+    print(type(b))
+    print(type(c))
+    import random
+    print(random.randrange(10,20))
+    x=int(3)
+    y=float(5)
+    z=int(3.9)
+    print(x)
+    print(y)
+    print(z)
+    z='''
+    My name is Mr Bones
+    I come from a country in Africa called South Africa
+    Within it there are two small countries'''
+    print(z)
+    w='Welcome to Multiverse'
+    print(w[14])
+    for x in 'multiverse':
+        print(x)
+    a='welcome'
+    print(len(a))
+    txt='Respect is the best thing to earn in this life'
+    print('mansion'in txt)
+    txt='The best things in life are not free'
+    if 'free'in txt:
+        print("Yes free is present")
+    txt='love will never fail'
+    if 'expensive'not in txt:
+        print("No 'expensive'is not present")    
---
+++ naivlek.py
@@ -0,0 +1,45 @@
+print("Hello World!")
+#My python practice
+print("Welcome to Python")
+""""
+what is multiverse
+i need to know today
+I will do it today
+"""
+print("Find out what is multiverse")
+if 5>2:
+    print("Five is greater than Two")
+    x=6
+    y="Naivlekarts"
+    print(x)
+    print(y)
+x=int(5)
+y=str("Phanthom")
+z=float(4)
+print(x,y,z)
+print(x)
+print(y)
+print(z)
+x=45
+y="Multiverse"
+print(type(x))
+print(type(y))
+cars=['mercedes benz','audi','bmw']
+a,b,c=cars
+print(a)
+print(b)
+print(c)
+a='Me '
+b='Myself'
+c=' and I'
+print(a+b+c)
+x=" Fantastic"
+y="Python is"
+def myfunc():
+    print(y+x)
+myfunc()
+def myfunc():
+    global y
+    y='flabberghasted'
+myfunc()
+print('I was '+y)
